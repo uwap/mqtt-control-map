30ad70a23710501aa29ff6ffcd35feac1052166b
uwap <me@uwap.name>
2017-11-01 03:24:03 +0100

    Add config folder

---
+++ config/utils.js
@@ -0,0 +1,5 @@
+// @flow
+
+export const rainbow = "rgba(200,120,120,0.5);"
+      + "background: linear-gradient(40deg, #FF0000 0%, #00FF00 50%, #0000FF 70%, #FFFF00 100%);"
+      + "background-clip: text; -webkit-background-clip: text;";
