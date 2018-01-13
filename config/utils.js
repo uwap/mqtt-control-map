a69552de7df0eb8e1b3c5c826c000732755ea4bf
uwap <me@uwap.name>
2018-01-13 20:05:47 +0100

    Fix rainbow colors

--- config/utils.js
+++ config/utils.js
@@ -1,5 +1,4 @@
 // @flow
 
 export const rainbow = "rgba(200,120,120,0.5);"
-      + "background: linear-gradient(40deg, #FF0000 0%, #00FF00 50%, #0000FF 70%, #FFFF00 100%);"
-      + "background-clip: text; -webkit-background-clip: text;";
+      + "--before-background: linear-gradient(40deg, #FF0000 0%, #00FF00 50%, #0000FF 70%, #FFFF00 100%);";
