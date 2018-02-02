06aeba9d7664e5aa4e4030762f8ab6261532bfc0
uwap <me@uwap.name>
2018-02-02 09:27:23 +0100

    Use /bin/sh -e

--- travis.sh
+++ travis.sh
@@ -1,7 +1,9 @@
-#!/bin/sh
-yarn lint && for conf in $(ls config/); do
+#!/bin/sh -e
+yarn lint
+for conf in $(ls config/); do
   if [ $conf == "utils.js" ]; then
     continue
   fi
-  yarn build $conf && yarn production-build $conf
-done || exit 1
+  yarn build $conf
+  yarn production-build $conf
+done
